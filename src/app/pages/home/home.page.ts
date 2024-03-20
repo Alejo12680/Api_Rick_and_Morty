a66ddfcc9464b7f984f0feaa36fb61adb92e358f
Alejo12680 <alejo152725@gmail.com>
2024-03-20 00:26:54 -0500

    Consumo de la api

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RickAndMortyService } from 'src/app/services/rick-and-morty.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  // Variable que es un Array de los personajes
+  public character: any [] = [];
+
+  // Variable que es un objeto que envia los parametros
+  public params = {} as any;
+
+  constructor( private rickAndMortySvc: RickAndMortyService) { }
 
   ngOnInit() {
+    // Se inicia la parametro con la pagina 0
+    this.params.page = 0;
+
+    // Cunado invocamos la funcion que suma 1 a la pagina al iniciar se empezaran con los primeros 20 personajes
+    this.getCharacter();
+  }
+
+  // Funcion que obtiene los personaje por evento
+  getCharacter(event?: any) {
+    // Cada vez que se ejecute esta funcion el parametro de la pagina sumara 1, se inicia en la pagina uno para obtener los primeros 20 personajes.
+    this.params.page += 1;
+
+    // Consumimos el servicio de la api, se suscribe
+    this.rickAndMortySvc.obtenerCharacter(this.params).subscribe({
+      next: (res: any) => {
+
+        // Revisamos la respuesta de la api en la cual hay un objeto que se llama 'results', como es un arreglo se necesita sacar los datos del arreglo y se puede hacer con sprit operaitor = (...)
+        this.character.push(...res.results);
+        console.log(this.character);        
+
+      },
+      error: (err: any) => {
+
+      }
+    })
   }
 
 }
---
+++ src/app/services/rick-and-morty.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RickAndMortyService {
+
+  constructor(private http: HttpClient) { }
+  
+  // Actualmente hay tres recursos que se pueden enviar como parametro: Character = Se usa para traer los personajes, Location = se usa para traer sus ubicaciones, Episode = se usa para traer los episodios.
+  obtenerCharacter(params: any) {
+    return this.http.get(environment.apiUrl + environment.character, {params})
+  }
+
+}
